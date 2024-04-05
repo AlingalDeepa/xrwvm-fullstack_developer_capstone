7fa3590ac344fd132284cc567edfa35ec1ebd3af
Deepa Alingal <deepaalingal922@gmail.com>
2024-04-05 02:41:36 -0400

    updated Linter corrections

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,8 +31,10 @@ DEBUG = True
 # ALLOWED_HOSTS=['localhost','https://deepaalingal-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 # CSRF_TRUSTED_ORIGINS=['https://deepaalingal-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 
-ALLOWED_HOSTS=['localhost','https://deepaalingal-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://deepaalingal-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost',
+                'https://deepaalingal-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = 
+                ['https://deepaalingal-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -147,4 +149,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
+
 
